5fb0aff3d377f34e6bd4bb651c33884aa7943552
IceHummmer <a.kucherenko000@gmail.com>
2026-09-24 18:46:55 +0300

    Keep authenticated API requests out of PWA cache

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE='payment-confirm-v8';
+const CACHE='payment-confirm-v9';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
+ const url=new URL(e.request.url);
+ if(url.origin!==self.location.origin)return;
  if(e.request.mode==='navigate'){
   e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));
   return;
